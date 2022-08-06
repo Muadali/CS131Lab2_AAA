c5aa11c03f23b639e6ed05dac64841fdf075e132
Muadali <108030259+Muadali@users.noreply.github.com>
2022-08-05 22:23:07 -0700

    Update Lockable.java

--- Lockable.java
+++ Lockable.java
@@ -1,16 +1,39 @@
 package CS131Lab2;
 
+/**
+ * This class represents a Lockable, 
+ * @author Abdifatah Abdi
+ * @version 0.1
+ * Programming lab 2
+ * Summer 2022
+ *
+ */
+
+//single line comment
+/*
+ * Multiple line comment
+ */
 public interface Lockable {
 
-		
+		/**
+		* setKey() method
+		*/
 		void setKey(int key);
 
+		
+		/**
+		* lock() method
+		*/
 		void lock(int key);
 
-		
+		/**
+		* unlock() method
+		*/
 		void unlock(int key);
 
-		
+		/**
+		* isLocked() method
+		*/
 		boolean isLocked();
 		
 	
